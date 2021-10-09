0929f92255a9019397ef7bf4214785656a982b43
Arman <ak682015@gmail.com>
2021-10-09 21:14:38 +0530

    Loader Added

--- chisfis/src/containers/PageHome/PageHome.tsx
+++ chisfis/src/containers/PageHome/PageHome.tsx
@@ -209,6 +209,8 @@ function PageHome() {
 
   const getSearchedData = async () => {
     try {
+      setIsLoaded(true);
+
       const q1 = query(
         collection(db, "searchedData"),
         where("searchLocationName", "==", "India")
@@ -250,10 +252,10 @@ function PageHome() {
         toData["searchLocationName"] = data.searchLocationName;
         setToTravelRest(toData);
       });
-      setIsLoaded(true);
     } catch (err) {
       console.log(err);
     }
+    setIsLoaded(false);
   };
 
   return (
@@ -417,10 +419,9 @@ function PageHome() {
         {/* <SectionOurFeatures /> */}
 
         {/* SECTION */}
-
-        {isLoaded && (
-          <div className='relative py-16'>
-            <BackgroundSection />
+        <div className='relative py-16'>
+          <BackgroundSection />
+          {!isLoaded ? (
             <div className={`nc-SectionSliderNewCategories relative`}>
               {/* <h3 className='text-2xl font-semibold'>Departure</h3> */}
               <div className='mt-6'>
@@ -620,8 +621,28 @@ function PageHome() {
                 </Tab.Group>
               </div>
             </div>
-          </div>
-        )}
+          ) : (
+            <div style={{ marginLeft: "50%" }}>
+              <svg
+                className='animate-spin -ml-1 mr-3 h-5 w-5'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'>
+                <circle
+                  className='opacity-25'
+                  cx='12'
+                  cy='12'
+                  r='10'
+                  stroke='currentColor'
+                  strokeWidth='3'></circle>
+                <path
+                  className='opacity-75'
+                  fill='currentColor'
+                  d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+              </svg>
+            </div>
+          )}
+        </div>
 
         {/* SECTION 1 */}
 
