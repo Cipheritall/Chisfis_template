f4c4fe9c9f10d360d3987311e77dbcfd6cb2036f
Arman <ak682015@gmail.com>
2021-10-22 00:29:22 +0530

    Test Region and test Type Added

--- chisfis/src/containers/ListingStayPage/ListingStayPage.tsx
+++ chisfis/src/containers/ListingStayPage/ListingStayPage.tsx
@@ -4,7 +4,7 @@ import SectionGridAuthorBox from "components/SectionGridAuthorBox/SectionGridAut
 import SectionHeroArchivePage from "components/SectionHeroArchivePage/SectionHeroArchivePage";
 import SectionSliderNewCategories from "components/SectionSliderNewCategories/SectionSliderNewCategories";
 import SectionSubscribe2 from "components/SectionSubscribe2/SectionSubscribe2";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, Fragment, useEffect, useState } from "react";
 import SectionGridFilterCard from "./SectionGridFilterCard";
 
 import { Helmet } from "react-helmet";
@@ -18,6 +18,13 @@ import WidgetTags from "containers/BlogPage/WidgetTags";
 import TabFilters from "containers/ListingExperiencesPage/TabFilters";
 import Pagination from "shared/Pagination/Pagination";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import Geocode from "react-geocode";
+import { query, collection, getDocs } from "@firebase/firestore";
+import { db } from "firebase";
+import { Popover, Transition } from "@headlessui/react";
+import ButtonPrimary from "shared/Button/ButtonPrimary";
+import ButtonThird from "shared/Button/ButtonThird";
+import Checkbox from "shared/Checkbox/Checkbox";
 
 export interface ListingStayPageProps {
   className?: string;
@@ -34,27 +41,144 @@ const ListingStayPage: FC<ListingStayPageProps> = ({
 
   const [showCentres, setShowCentres] = useState(false);
   const [region, setRegion] = useState(null);
+  const [latLon, setLatLon] = useState({});
+
+  const [testCentres, setAllTestCentres] = useState([]);
+  const [testProviders, setAllTestProviders] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
+    getAllTestCentres();
     if (history.location.state) {
       setShowCentres(true);
     }
   }, []);
 
-  const handleRegionChange = (value) => {
-    console.log(value);
-  };
-
-  const setRegionSel = () => {
-    setRegion("Hi");
+  const getAllTestCentres = async () => {
+    setIsLoaded(true);
+    const q1 = query(collection(db, "testCentres"));
+    const q2 = query(collection(db, "testProviders"));
+    const querySnapshot1 = await getDocs(q1);
+    const querySnapshot2 = await getDocs(q2);
+    let testCen = [];
+    let testPro = [];
+    querySnapshot1.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      testCen.push(doc.data());
+    });
+    querySnapshot2.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      testPro.push(doc.data());
+    });
+    setAllTestCentres(testCen);
+    setAllTestProviders(testPro);
+    console.log(testCen);
+    setIsLoaded(false);
   };
 
   const onLocationSelect = (e) => {
     setRegion(e);
     console.log(region);
     setShowCentres(true);
+    Geocode.setApiKey("AIzaSyDNfNT3Ds4oOEXvnzX0LJSDDeJEuzll8ys");
+    Geocode.setLanguage("en");
+    Geocode.enableDebug();
+    Geocode.fromAddress(e.label).then(
+      (response) => {
+        const { lat, lng } = response.results[0].geometry.location;
+        setLatLon({ lat, lng });
+        console.log(lat, lng);
+        findNearTest(19.0759837, 72.8776559);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   };
 
+  const testCentress = [
+    {
+      name: "t1",
+      lat: 19.090326792232343,
+      lng: 72.83746502202662,
+    },
+    {
+      name: "t1",
+      lat: 19.08070130222621,
+      lng: 72.8701366333966,
+    },
+    {
+      name: "t2",
+      lat: 28.70089677183948,
+      lng: 77.2154385236084,
+    },
+  ];
+  const findNearTest = (lat, lng) => {
+    let arr = [];
+    testCentres.map((tc) =>
+      arr.push(getDistanceBetween(lat, lng, tc.latitude, tc.longitude, "Km"))
+    );
+    console.log(arr);
+  };
+
+  const getDistanceBetween = (lat1, lon1, lat2, lon2, unit) => {
+    var p = 0.017453292519943295; // Math.PI / 180
+    var c = Math.cos;
+    var a =
+      0.5 -
+      c((lat2 - lat1) * p) / 2 +
+      (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+
+    // let theta = lng1 - lng2;
+    // let distance =
+    //   Math.sin((lat1 * Math.PI) / 180) * Math.sin((lat2 * Math.PI) / 180) +
+    //   Math.cos((lat1 * Math.PI) / 180) *
+    //     Math.cos((lat2 * Math.PI) / 180) *
+    //     Math.cos((theta * Math.PI) / 180);
+    // distance = Math.acos(distance);
+    // distance = (distance * Math.PI) / 180;
+    // distance = distance * 60 * 1.515 * 1.609344;
+    // return distance;
+  };
+
+  const renderXClear = () => {
+    return (
+      <span className='w-4 h-4 rounded-full bg-primary-500 text-white flex items-center justify-center ml-3 cursor-pointer'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='h-3 w-3'
+          viewBox='0 0 20 20'
+          fill='currentColor'>
+          <path
+            fillRule='evenodd'
+            d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+            clipRule='evenodd'
+          />
+        </svg>
+      </span>
+    );
+  };
+  const renderTabsPriceRage = () => {
+    return (
+      <>
+        {testProviders.map((pro) => (
+          <Popover className='relative'>
+            {({ open, close }) => (
+              <>
+                <Popover.Button
+                  className={`flex items-center justify-center px-4 py-2 text-sm rounded-full border border-primary-500 bg-primary-50 text-primary-700 focus:outline-none `}>
+                  <span>{pro.providerName}</span>
+                  {renderXClear()}
+                </Popover.Button>
+              </>
+            )}
+          </Popover>
+        ))}
+      </>
+    );
+  };
   return (
     <div
       className={`nc-ListingStayPage relative overflow-hidden ${className}`}
@@ -115,7 +239,12 @@ const ListingStayPage: FC<ListingStayPageProps> = ({
         {showCentres && (
           <>
             <div className='mb-8 lg:mb-11'>
-              <TabFilters />
+              {/* <TabFilters /> */}
+              <div className='flex lg:space-x-4'>
+                <div className='hidden lg:flex space-x-4'>
+                  {renderTabsPriceRage()}
+                </div>
+              </div>
             </div>
             <div className='grid grid-cols-1 gap-6 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
               {data.map((stay) => (
--- chisfis/src/containers/ListingStayPage/SelectTestPage.tsx
+++ chisfis/src/containers/ListingStayPage/SelectTestPage.tsx
@@ -1,6 +1,6 @@
 import { Tab } from "@headlessui/react";
 import { PencilAltIcon } from "@heroicons/react/outline";
-import React, { FC, Fragment, useState } from "react";
+import React, { FC, Fragment, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import visaPng from "images/vis.png";
 import mastercardPng from "images/mastercard.svg";
@@ -15,6 +15,8 @@ import Select from "shared/Select/Select";
 import FormItem from "./FormItem";
 import { Link } from "react-router-dom";
 import Heading2 from "components/Heading/Heading2";
+import { query, collection, where, getDocs } from "@firebase/firestore";
+import { db } from "firebase";
 
 export interface CheckOutPageProps {
   className?: string;
@@ -67,34 +69,45 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
 
   const history = useHistory();
 
-  const regionOptions = [
-    { value: "United Kingdom", label: "United Kingdom" },
-    { value: "United States", label: "United States" },
-    { value: "Mexico", label: "Mexico" },
-    { value: "Spain", label: "Spain" },
-    { value: "Portugal", label: "Portugal" },
-    { value: "UAE", label: "UAE" },
-    { value: "France", label: "France" },
-  ];
-
   const journeyTypeOptions = [
     { value: "Hotel", label: "Departing (Pre-departure)" },
     { value: "Private room", label: "Returning (Pre-Return)" },
     { value: "Private room", label: "Already Arrived (After-Return)" },
   ];
 
-  const testTypeOptions = [
-    { value: "Day 2" },
-    { value: "Day 8" },
-    { value: "PCR" },
-    { value: "Antigen" },
-    { value: "LAMP" },
-    { value: "Antibody" },
-  ];
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  const [regionOptions, setRegionOptions] = useState([]);
   const [region, setRegion] = useState("United Kingdom");
   const [journeyType, setJourneyType] = useState("Departing");
+  const [testTypeOptions, setTestTypeOptions] = useState([]);
   const [testType, setTestType] = useState("Day 2");
+
+  useEffect(() => {
+    getAllOptions();
+  }, []);
+
+  const getAllOptions = async () => {
+    setIsLoaded(true);
+    const q1 = query(collection(db, "bookatestdestinations"));
+    const q2 = query(collection(db, "bookatesttypes"));
+    const querySnapshot1 = await getDocs(q1);
+    const querySnapshot2 = await getDocs(q2);
+    let regions = [];
+    let testTypes = [];
+    querySnapshot1.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      regions.push(doc.data().location);
+    });
+    querySnapshot2.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      testTypes.push(doc.data().testType);
+    });
+    setRegionOptions(regions);
+    setTestTypeOptions(testTypes);
+    console.log(testTypes);
+    setIsLoaded(false);
+  };
 
   const handleRegionChange = (e) => {
     setRegion(e.target.value);
@@ -124,100 +137,126 @@ const CheckOutPage: FC<CheckOutPageProps> = ({ className = "" }) => {
             </span>
           }
         />
-        <div className='w-full flex flex-col sm:rounded-2xl sm:border border-neutral-200 dark:border-neutral-700 space-y-8 px-0 sm:p-6 xl:p-8'>
-          <form onSubmit={handleSubmit}>
-            <div className='space-y-8'>
-              {/* ITEM */}
+        {!isLoaded ? (
+          <div className='w-full flex flex-col sm:rounded-2xl sm:border border-neutral-200 dark:border-neutral-700 space-y-8 px-0 sm:p-6 xl:p-8'>
+            <form onSubmit={handleSubmit}>
+              <div className='space-y-8'>
+                {/* ITEM */}
 
-              <FormItem
-                label='Select the region you will be taking your test in'
-                desc='Please select the country or region that you will be taking the test in, so that we can offer you the right services'>
-                <select
-                  value={region}
-                  onChange={handleRegionChange}
-                  className={`nc-Select h-11 block w-full text-sm rounded-2xl border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900`}>
-                  {regionOptions.map((region) => (
-                    <option key={region.value} value={region.value}>
-                      {region.value}
-                    </option>
-                  ))}
-                </select>
-              </FormItem>
-              <FormItem
-                label='Arriving or Departing'
-                desc='Are you going way, coming home or already back home?'>
-                <select
-                  value={journeyType}
-                  onChange={handleJourneyTypeChange}
-                  className={`nc-Select h-11 block w-full text-sm rounded-2xl border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900`}>
-                  {journeyTypeOptions.map((journeyType) => (
-                    <option value={journeyType.value}>
-                      {journeyType.label}
-                    </option>
-                  ))}
-                </select>
-              </FormItem>
-              <FormItem
-                label='Vaccination Status'
-                desc='Please tell us about your vaccination status'>
-                <Select>
-                  <option value='Hotel'>Not Vaccinated</option>
-                  <option value='Private room'>1 Dose</option>
-                  <option value='Private room'>Fully Vaccinated</option>
-                </Select>
-              </FormItem>
-              <FormItem
-                label='Resident or Visitor'
-                desc='Where you a resident of the region or are you visiting?'>
-                <Select>
-                  <option value='Resident'>Resident</option>
-                  <option value='Visitor'>Visitor</option>
-                </Select>
-              </FormItem>
-              <FormItem
-                label='Center or Self-test'
-                desc='Where will you be taking the test?'>
-                <Select>
-                  <option value='Self-test'>Self-test</option>
-                  <option value='Center'>At a Test Center</option>
-                </Select>
-              </FormItem>
+                <FormItem
+                  label='Select the region you will be taking your test in'
+                  desc='Please select the country or region that you will be taking the test in, so that we can offer you the right services'>
+                  <select
+                    value={region}
+                    onChange={handleRegionChange}
+                    className={`nc-Select h-11 block w-full text-sm rounded-2xl border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900`}>
+                    {regionOptions.map((region) => (
+                      <option key={region} value={region}>
+                        {region}
+                      </option>
+                    ))}
+                  </select>
+                </FormItem>
+                <FormItem
+                  label='Arriving or Departing'
+                  desc='Are you going way, coming home or already back home?'>
+                  <select
+                    value={journeyType}
+                    onChange={handleJourneyTypeChange}
+                    className={`nc-Select h-11 block w-full text-sm rounded-2xl border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900`}>
+                    {journeyTypeOptions.map((journeyType) => (
+                      <option value={journeyType.value}>
+                        {journeyType.label}
+                      </option>
+                    ))}
+                  </select>
+                </FormItem>
+                <FormItem
+                  label='Vaccination Status'
+                  desc='Please tell us about your vaccination status'>
+                  <Select>
+                    <option value='Hotel'>Not Vaccinated</option>
+                    <option value='Private room'>1 Dose</option>
+                    <option value='Private room'>Fully Vaccinated</option>
+                  </Select>
+                </FormItem>
+                <FormItem
+                  label='Resident or Visitor'
+                  desc='Where you a resident of the region or are you visiting?'>
+                  <Select>
+                    <option value='Resident'>Resident</option>
+                    <option value='Visitor'>Visitor</option>
+                  </Select>
+                </FormItem>
+                <FormItem
+                  label='Center or Self-test'
+                  desc='Where will you be taking the test?'>
+                  <Select>
+                    <option value='Self-test'>Self-test</option>
+                    <option value='Center'>At a Test Center</option>
+                  </Select>
+                </FormItem>
 
-              <FormItem
-                label='Test Type'
-                desc='We offer the full range of tests to depart out of as well as return from the region'>
-                <select
-                  value={testType}
-                  onChange={handleTestTypeChange}
-                  className={`nc-Select h-11 block w-full text-sm rounded-2xl border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900`}>
-                  {testTypeOptions.map((testType) => (
-                    <option value={testType.value}>{testType.value}</option>
-                  ))}
-                </select>
-              </FormItem>
-              <FormItem
-                label='Service Type'
-                desc='How fast do you want the results?'>
-                <Select>
-                  <option value='Same Day'>
-                    Same Day (Results by mid-night same day)
-                  </option>
-                  <option value='Next Day'>
-                    Next Day (Results by mid-night next day)
-                  </option>
-                  <option value='Express'>Express (Results in 1-3 hrs)</option>
-                  <option value='Hour'>In an hour (Results in 60 mins)</option>
-                </Select>
-              </FormItem>
-            </div>
-            <div className='pt-4'>
-              {/* <Link to='/test-centers'>
+                <FormItem
+                  label='Test Type'
+                  desc='We offer the full range of tests to depart out of as well as return from the region'>
+                  <select
+                    value={testType}
+                    onChange={handleTestTypeChange}
+                    className={`nc-Select h-11 block w-full text-sm rounded-2xl border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900`}>
+                    {testTypeOptions.map((testType) => (
+                      <option value={testType}>{testType}</option>
+                    ))}
+                  </select>
+                </FormItem>
+                <FormItem
+                  label='Service Type'
+                  desc='How fast do you want the results?'>
+                  <Select>
+                    <option value='Same Day'>
+                      Same Day (Results by mid-night same day)
+                    </option>
+                    <option value='Next Day'>
+                      Next Day (Results by mid-night next day)
+                    </option>
+                    <option value='Express'>
+                      Express (Results in 1-3 hrs)
+                    </option>
+                    <option value='Hour'>
+                      In an hour (Results in 60 mins)
+                    </option>
+                  </Select>
+                </FormItem>
+              </div>
+              <div className='pt-4'>
+                {/* <Link to='/test-centers'>
                 <ButtonPrimary>Continue</ButtonPrimary>
               </Link> */}
-              <ButtonPrimary type='submit'>Continue</ButtonPrimary>
-            </div>
-          </form>
-        </div>
+                <ButtonPrimary type='submit'>Continue</ButtonPrimary>
+              </div>
+            </form>
+          </div>
+        ) : (
+          <div style={{ marginLeft: "50%" }} className='py-6'>
+            <svg
+              className='animate-spin -ml-1 mr-3 h-5 w-5'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'>
+              <circle
+                className='opacity-25'
+                cx='12'
+                cy='12'
+                r='10'
+                stroke='currentColor'
+                strokeWidth='3'></circle>
+              <path
+                className='opacity-75'
+                fill='currentColor'
+                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+            </svg>
+          </div>
+        )}
       </>
     );
   };
