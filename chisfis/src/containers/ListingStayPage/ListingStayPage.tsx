af59e591f5c160421a77298ff035a17091af9ea3
Arman <ak682015@gmail.com>
2021-10-26 22:41:54 +0530

    filters Added

--- chisfis/src/containers/ListingStayPage/ListingStayPage.tsx
+++ chisfis/src/containers/ListingStayPage/ListingStayPage.tsx
@@ -328,8 +328,9 @@ const ListingStayPage: FC<ListingStayPageProps> = ({
               <div className='mb-8 lg:mb-11'>
                 <div className='flex lg:space-x-4'>
                   <div className='hidden lg:flex space-x-4'>
-                    {renderTabsPriceRage()}
-                  </div>
+                    {/* {renderTabsPriceRage()} */}
+                    <TabFilters />
+                    </div>
                 </div>
               </div>
               <div className='grid grid-cols-1 gap-6 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
